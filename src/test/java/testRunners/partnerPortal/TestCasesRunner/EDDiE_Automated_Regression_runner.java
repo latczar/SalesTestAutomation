8d821f73dc4e6948d989a2410b7de0188763f6ed
Lat Caesar <lcaesar@sunpowercorp.com>
2022-08-03 01:25:58 +0800

    update Regression TestRunner files

--- src/test/java/testRunners/partnerPortal/TestCasesRunner/EDDiE_Automated_Regression_runner.java
+++ src/test/java/testRunners/partnerPortal/TestCasesRunner/EDDiE_Automated_Regression_runner.java
@@ -22,24 +22,24 @@ import pageObjects.partnerportal.home.BasePage;
 				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Incentives and Credit/DCRM_Sales-93594 - NJ_FederalTaxCredit26_Validation.feature",
 				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Incentives and Credit/DCRM_Sales-93597 - State Tax Credit MA.feature",
 				   //Mounting Type
-				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Mounting Type/DCRM_Sales-93504 - Non-sunpower Ground Mount",
+				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Mounting Type/DCRM_Sales-93504 - Non-sunpower Ground Mount.feature",
 				   //Save Changes
-				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Save Changes/DCRM_Sales-93582 - SPD_MinimumGrossPrice_Validation",
-				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Save Changes/DCRM_Sales-99950 - ND_Lease_MinPPW_Validation",
+				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Save Changes/DCRM_Sales-93582 - SPD_MinimumGrossPrice_Validation.feature",
+				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Save Changes/DCRM_Sales-99950 - ND_Lease_MinPPW_Validation.feature",
 				   //Save Changes - Adders and Discount
-				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Save Changes/Adders and Discount/DCRM_Sales-93573 - SPD_Front Edge Skirt",
-				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Save Changes/Adders and Discount/DCRM_Sales-93574 - SPD_Critter Guard",
+				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Save Changes/Adders and Discount/DCRM_Sales-93573 - SPD_Front Edge Skirt.feature",
+				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Save Changes/Adders and Discount/DCRM_Sales-93574 - SPD_Critter Guard.feature",
 				   //Save Design
-				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Save Design/DCRM_Sales-98870 - SPD_MA_SystemSize_Validation",
-				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Save Design/DCRM_Sales-98969 - SPD_IL_SystemSize_Validation",
-				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Save Design/DCRM_Sales-98971 - SPD_QuoteName_Validation",
+				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Save Design/DCRM_Sales-98870 - SPD_MA_SystemSize_Validation.feature",
+				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Save Design/DCRM_Sales-98969 - SPD_IL_SystemSize_Validation.feature",
+				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Save Design/DCRM_Sales-98971 - SPD_QuoteName_Validation.feature",
 				   //Savings Tab validation
-				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Savings Tab validation/DCRM_Sales-93497 - 100k validation",
-				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Savings Tab validation/DCRM_Sales-93498 - 8_perWatt_Loan_withoutStorage_validation",
-				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Savings Tab validation/DCRM_Sales-93499 - 10_perWatt_Loan_withStorage_validation",
+				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Savings Tab validation/DCRM_Sales-93497 - 100k validation.feature",
+				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Savings Tab validation/DCRM_Sales-93498 - 8_perWatt_Loan_withoutStorage_validation.feature",
+				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Savings Tab validation/DCRM_Sales-93499 - 10_perWatt_Loan_withStorage_validation.feature",
 				   //Types of Pricing Input
-				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Types of Pricing Input/DCRM_Sales-93492 - FO PPW_Validation",
-				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Types of Pricing Input/DCRM_Sales-93494 - Cash GrossPrice_Validation"
+				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Types of Pricing Input/DCRM_Sales-93492 - FO PPW_Validation.feature",
+				   "src/main/resources/features/spwr/EDDiE Automated Manual Regression/Types of Pricing Input/DCRM_Sales-93494 - Cash GrossPrice_Validation.feature"
 		},		
 		glue = {"stepDefinitions"},
 		stepNotifications = true,
